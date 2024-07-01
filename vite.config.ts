b4c3a642191b360f14c33498d8ee58042ee59212
ygr99 <196156709@qq.com>
2024-07-01 10:16:33 +0800

    Committing changes before switching to gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
     // 页面访问路径不是根目录的时候要改
-    base: '/',
+    base:'./',// 将根路径换成相对路径
     plugins: [vue()],
     resolve: {
         alias: {
